556952370458bd2c202855b54a3fcf93a1ed4d48
Maks <lomic2004@yandex.ru>
2023-11-14 18:06:30 +0300

    http get and post requests realized

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String url = "https://reqres.in/api/users";
+        try {
+            URL myUrl = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) myUrl.openConnection();
+            connection.setRequestMethod("GET");
+            System.out.println(connection.getResponseCode());
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String response = in.readLine();
+            in.close();
+            System.out.println(response);
+            connection.disconnect();
+            URL url1 = new URL("https://reqres.in/api/users");
+            HttpURLConnection connection1 = (HttpURLConnection) url1.openConnection();
+            connection1.setRequestMethod("POST");
+            connection1.setRequestProperty("Content-Type", "application/json");
+            connection1.setDoOutput(true);
+            String body = "{\n\"name\": \"Maks\",\n\"job\": \"student\"\n}";
+            BufferedOutputStream wr = new BufferedOutputStream(connection1.getOutputStream());
+            wr.write(body.getBytes());
+            wr.close();
+            BufferedReader in1 = new BufferedReader(new InputStreamReader(connection1.getInputStream(), "utf-8"));
+            response = in1.readLine();
+            in1.close();
+            System.out.println(response);
+        } catch (MalformedURLException e) {
+            System.out.println("Bad request");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
